6c512f29d1a6c3842173daa9b398671c2e7a8e85
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-01-06 10:55:00 -0200

    master implementation

---
+++ multipleMasterDetail/masterTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  masterTableViewController.h
+//  multipleMasterDetail
+//
+//  Created by Bruno on 1/6/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface masterTableViewController : UITableViewController
+
+@property NSMutableArray *masterSource;
+
+@end
